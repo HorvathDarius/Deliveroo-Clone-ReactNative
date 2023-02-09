bf7091fa075f3c466b99e187759501fc04c180f2
Darius Horvath <dariushorvath33@gmail.com>
2023-02-09 23:19:12 +0200

    Commit before the sanity connection to frontend

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './.expo/screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'jdu3pyv9',
+    dataset: 'production'
+  }
+})
